760f39c2ae6ce784dae39198f66eeed339dfae04
lucas <lucrosinol@gmail.com>
2022-04-13 18:17:14 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my_dofus_app_client';
+}
